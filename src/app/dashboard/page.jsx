cb0f27fc0a2861ee7325425190a9516ceb5a2455
Dheeraj <dheeraj.vp2023@vitstudent.ac.in>
2025-03-09 11:46:37 +0530

    test commit 3

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,9 @@
 const Dashboard = () => {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <div className="p-6 shadow-lg rounded-lg text-lg font-semibold">
-          Dashboard
+      <div className="flex items-center justify-center min-h-screen bg-black text-white">
+        <div className="p-6 bg-gray-900 shadow-lg rounded-lg text-lg font-semibold">
+          <h1 className="text-center text-2xl mb-2">CalQuity Dashboard</h1>
+          <p>Welcome back!</p>
         </div>
       </div>
     )
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,9 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-6 shadow-lg rounded-lg">
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="p-6 bg-gray-900 shadow-lg rounded-lg">
+        <h1 className="text-center text-xl font-bold mb-4">Join CalQuity Today</h1>
         <SignUp />
       </div>
     </div>
