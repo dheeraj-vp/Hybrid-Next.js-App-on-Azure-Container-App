94cc179f0fcc452b47e9ba911b6b22c46f337f8a
Dheeraj <dheeraj.vp2023@vitstudent.ac.in>
2025-03-09 11:33:21 +0530

    test commit

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,11 @@
-const Dashboard = () =>{
+const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-200">
+        <div className="p-6 bg-white shadow-lg rounded-lg text-lg font-semibold">
+          Dashboard
+        </div>
+      </div>
     )
-}
-export default Dashboard;
+  }
+  export default Dashboard;
+  
